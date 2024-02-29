3884d6aaaa4c93254e4dcfcf4c9670710f6297bf
Hamzasheikh <159949442+Hamzasheikh866@users.noreply.github.com>
2024-02-29 14:59:23 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,3 @@
+var quote = "A person never made a mistake never tried to anything";
+var author = "Albert Einstine";
+console.log("".concat(author, " Once said, ").concat(quote));
---
+++ app.ts
@@ -0,0 +1,3 @@
+let quote: string = "A person never made a mistake never tried to anything";
+let author: string = "Albert Einstine";
+console.log(`${author} Once said, ${quote}`);
